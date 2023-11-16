88f4773620890a8be173e54c6bd01a1734903813
Aryan K <101513901+aryanbk@users.noreply.github.com>
2023-11-17 02:51:05 +0530

    Extract URL path

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,18 +7,39 @@ import java.net.Socket;
 public class Main {
   public static void main(String[] args) {
     System.out.println("Logs from your program will appear here!");
-    try {
-      ServerSocket serverSocket = new ServerSocket(4221);
-
+    try (ServerSocket serverSocket = new ServerSocket(4221)) {
       serverSocket.setReuseAddress(true);
-      int spinCount = 0;
 
       while (true) {
         try (Socket clienSocket = serverSocket.accept()) {
-          String httpResponse = "HTTP/1.1 200 OK\r\n\r\n";
-          clienSocket.getOutputStream().write(httpResponse.getBytes("UTF-8"));
+          String httpResponse200 = "HTTP/1.1 200 OK\r\n\r\n";
+          String httpResponse404 = "HTTP/1.1 404 Not Found\r\n\r\n";
+
+          InputStreamReader isr = new InputStreamReader(clienSocket.getInputStream());
+          BufferedReader reader = new BufferedReader(isr);
+          String line = reader.readLine();
+
+          for (int lineNumber = 0; !line.isEmpty(); ++lineNumber) {
+            if (lineNumber == 0) {
+              String[] parts = line.split("\r\n");
+              String urlPath;
+
+              if (parts[0].startsWith("GET")) {
+                urlPath = parts[0].split(" ")[1];
+                if (urlPath.equals("/")) {
+                  clienSocket.getOutputStream().write(httpResponse200.getBytes("UTF-8"));
+                } else {
+                  clienSocket.getOutputStream().write(httpResponse404.getBytes("UTF-8"));
+                }
+              }
+            }
+
+            line = reader.readLine();
+          }
+
+          // clienSocket.getOutputStream().write(httpResponse200.getBytes("UTF-8"));
         } catch (Exception e) {
-          System.out.println(e);
+          System.out.println("ClientSocket Exception: " + e.getMessage());
         }
       }
 
